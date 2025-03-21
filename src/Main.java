e097caa5c8286f84760decf07472a455654dcf35
Matheus Aguiar <matheusaguiardealmeida@gmail.com>
2025-03-20 21:58:32 -0300

    Working with arrayList on java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import br.com.alura.screenmatch.models.Movie;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Movie avengers = new Movie();
@@ -7,5 +9,18 @@ public class Main {
         avengers.setYearOfRelease(2010);
         avengers.setDurationInMinutes(200);
         avengers.showTechnicalSheet();
+
+        Movie avatar = new Movie();
+        avatar.setName("Avatar");
+        avatar.setYearOfRelease(2000);
+        avatar.setDurationInMinutes(200);
+
+
+        ArrayList<Movie> moviesList = new ArrayList<>();
+        moviesList.add(avengers);
+        moviesList.add(avatar);
+
+        System.out.println("List size: " + moviesList.size());
+        System.out.println("Primeiro filme: " + moviesList.getFirst().getName());
     }
 }
